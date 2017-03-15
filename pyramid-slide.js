d4055f28a771ce474b08f43cd07c21c45be3327e
Ryan Sutton <ryanchristiansutton@gmail.com>
2017-03-14 21:11:16 -0500

    added functionality to drawPyramid() to change brick symbol and to draw image at loading of page

--- pyramid-slide.js
+++ pyramid-slide.js
@@ -1,40 +1,33 @@
-
-
-var heightElem = document.getElementById("height");
-var formElem = document.getElementById("draw-form");
-
-//set an oninput function for the height slider
-formElem.oninput = function(event)
-    event.drawPyramid(heightElem)
-
-
-
+window.onload = drawPyramid()
 
 /**
  * drawPyramid
  *
  * Renders, in the HTML document, a pyramid of the specified height
  */
-function drawPyramid(height) {
-
+function drawPyramid() {
+    var heightElem = document.getElementById("height");
+    var brickElem= document.getElementById("brick");
     // first, clear the old content
     document.getElementById("pyramid").innerHTML = "";
 
     // for each row....
-    for (var row = 0; row < height; row++) {
+    for (var row = 0; row < heightElem.value; row++) {
 
         // figure out number of bricks and spaces
         var numBricks = row + 2;
-        var numSpaces = height - row - 1;
+        var numSpaces = heightElem.value - row - 1;
 
         // build up a string for this row
+        // debugger;
         var rowStr = "";
         for (var i = 0; i < numSpaces; i++) {
             var spaceChar = "&nbsp"; // this is the HTML encoding for a space " "
             rowStr += spaceChar;
         }
+
         for (var i = 0; i < numBricks; i++) {
-            rowStr += "#";
+            rowStr += brickElem.value;
         }
 
         // make a <p> element for this row, and insert it into the #pyramid container
